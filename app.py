c302453a7f11aef319daed1601f5fa956f3eac7c
Sheyne Anderson <sheyne@sheyne.com>
2020-01-06 23:36:30 +0000

    I am a bad person

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 import sys
 import json
 from flask_heroku import Heroku
-from datetime import datetime
+import datetime
 import os
 
 app = Flask( __name__ )
-app.config.from_object(os.environ['DATABASE_URL'])
+app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 heroku = Heroku(app)
@@ -18,11 +18,10 @@ class ArrivalTime(db.Model):
     __tablename__ = "ArrivalTime"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.Text())
-    time = db.Column(db.DateTime())
+    time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def __init__ (self, email):
         self.email = email
-        self.time = datetime.now()
 
 
 @app.route("/late", methods=["GET"])
@@ -33,6 +32,10 @@ def post_to_db():
     db.session.add(indata)
     db.session.commit()
     return 'Success!'
+
+@app.route("/list", methods=["GET"])
+def _slash_list():
+    return "<br />".join(f"{e.email}: {e.time}" for e in ArrivalTime.query.all())
 
 if __name__ == ' __main__':
     app.debug = True
